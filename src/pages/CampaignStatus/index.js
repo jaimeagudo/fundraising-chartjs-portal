65ca897e07dc11193cd1f91eb0f1545fd1f45e41
jaimeagudo <jaimeagudo@users.no.reply.github.com>
2020-10-02 17:30:16 +0200

    Add nice overall progress counter

--- src/pages/CampaignStatus/index.js
+++ src/pages/CampaignStatus/index.js
@@ -1,14 +1,20 @@
 import React, { useState, useEffect, memo } from 'react';
 import { Helmet } from 'react-helmet';
 import { Line, Bar, Doughnut } from 'react-chartjs-2'
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withTheme } from '@material-ui/core/styles';
 import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import Page from 'material-ui-shell/lib/containers/Page/Page'
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Scrollbar from 'material-ui-shell/lib/components/Scrollbar/Scrollbar'
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import Group from '@material-ui/icons/Group';
+
 import efpApiClient from '../../services/efpApiClient';
 import { ObjectRenderer, ArrayRenderer } from 'components/Generic'
+import CountUp from 'react-countup'
+
+
 import api from '../../config/api'
 
 import { useIntl, FormattedMessage } from 'react-intl'
@@ -37,7 +43,8 @@ const useStyles = makeStyles({
 });
 
 
-export function CampaignStatus() {
+function CampaignStatus({ theme }) {
+
     const intl = useIntl()
 
     const [stats, setStats] = useState(null);
@@ -64,6 +71,35 @@ export function CampaignStatus() {
                 <title>{intl.formatMessage({ id: 'campaignStatus' })}</title>
             </Helmet>
             <Scrollbar style={{ height: '100%', width: '100%', display: 'flex', flex: 1 }} >
+                <div style={{ display: 'flex', flexDirection: 'row', margin: 30 }}>
+                    <CountUp
+                        style={{
+                            fontSize: 60,
+                            // color: theme.palette.primary.main,
+                            // fontFamily: theme.fontFamily
+                        }}
+                        separator=','
+                        prefix="£"
+                        start={0}
+                        end={status && status.raisedAmountTomorrow && status.raisedAmountTomorrow / 100}
+                    />
+                    <div>
+                        <AccountBalanceIcon color="primary" className="material-icons" style={{ fontSize: 70, marginLeft: 16 }} />
+                    </div>
+                    <CountUp
+                        style={{
+                            fontSize: 60,
+                            // color: theme.palette.primary.main,
+                            // fontFamily: theme.fontFamily
+                        }}
+                        separator=','
+                        start={0}
+                        end={status && status.investorsCountTomorrow}
+                    />
+                    <div>
+                        <Group color="primary" className="material-icons" style={{ fontSize: 70, marginLeft: 16 }} />
+                    </div>
+                </div>
                 {status ? <ObjectRenderer key='status' name={'Status'} obj={status} fieldsWithPences={api.fieldsWithPences} classes={classes} /> : null}
                 {stats ? renderObj(stats) : null}
                 <FormControl component="fieldset" error={!!error} className={classes.formControl}>
@@ -74,4 +110,4 @@ export function CampaignStatus() {
     )
 }
 
-export default memo(CampaignStatus);
+export default memo(withTheme(CampaignStatus));
