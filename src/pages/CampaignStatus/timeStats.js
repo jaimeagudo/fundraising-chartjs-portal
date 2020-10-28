8bdede7779beb79aafc2c7b12e82c776d3331d20
jaimeagudo <jaimeagudo@users.no.reply.github.com>
2020-10-28 12:14:34 +0100

    Eslint timeStats page

--- src/pages/CampaignStatus/timeStats.js
+++ src/pages/CampaignStatus/timeStats.js
@@ -1,32 +1,21 @@
 import React, { useState, useEffect, memo } from 'react';
-// import { Bar } from '@reactchartjs/react-chart.js'
 import { Helmet } from 'react-helmet';
 import { Bar } from 'react-chartjs-2';
+import { useIntl } from 'react-intl'
+
+import { makeStyles, withTheme } from '@material-ui/core/styles';
 import Switch from '@material-ui/core/Switch';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Paper from '@material-ui/core/Paper';
-
-import { useIntl, FormattedMessage } from 'react-intl'
-
-
-import { makeStyles, withTheme } from '@material-ui/core/styles';
-
 import Page from 'material-ui-shell/lib/containers/Page/Page'
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Scrollbar from 'material-ui-shell/lib/components/Scrollbar/Scrollbar'
 
 
 import efpApiClient from '../../services/efpApiClient';
-
 import useSessionTimeoutHandler from 'hooks/useSessionTimeoutHandler'
-import { prettifyKV, fixedColors } from '../../utils'
-import { ObjectRenderer, ArrayRenderer } from 'components/Generic'
-
-
-
-const stacked = false
-
-
+import { prettifyKV, } from '../../utils'
+import { ArrayRenderer } from 'components/Generic'
 
 
 
@@ -64,24 +53,16 @@ function TimeStats({ theme }) {
 
     const options = {
         scales: {
-            yAxes: [
-                {
-                    stacked,
-                    ticks: {
-                        beginAtZero: true,
-                    },
-                },
-            ],
-            xAxes: [
-                {
-                    stacked,
-                },
-            ],
+            yAxes: [{
+                stacked,
+                ticks: { beginAtZero: true }
+            }],
+            xAxes: [{
+                stacked
+            }],
         },
         maintainAspectRatio: true,
-
     }
-
 
     const gbpTooltips = {
         tooltips: {
@@ -91,15 +72,17 @@ function TimeStats({ theme }) {
         },
     }
 
-    const handleSwitch = (event) => {
-        setStacked(event.target.checked);
-    };
-    console.log("TimeStats -> window.innerHeight", window.innerHeight)
+    const handleSwitch = (event) => setStacked(event.target.checked);
 
     const isMobile = window.innerWidth < 500
-
-
     const responsiveSizeHack = isMobile ? 200 : null
+
+    const control = <Switch
+        checked={stacked}
+        onChange={handleSwitch}
+        name="checkedB"
+        color="primary"
+    />
 
     return (
         <Page pageTitle={intl.formatMessage({ id: 'campaignTimeStats' })}>
@@ -107,18 +90,11 @@ function TimeStats({ theme }) {
                 <title>{intl.formatMessage({ id: 'campaignTimeStats' })}</title>
             </Helmet>
             <Scrollbar >
-
                 {stats ? stats.map(stat =>
                     (<Paper key={stat.title} className={classes.paper} >
                         {stat.title && <h2 className={classes.title}>{stat.title}</h2>}
                         <FormControlLabel
-                            control={<Switch
-                                checked={stacked}
-                                onChange={handleSwitch}
-                                name="checkedB"
-                                color="primary"
-                            />
-                            }
+                            control={control}
                             label="Stacked"
                         />
                         <Bar data={stat}
@@ -126,7 +102,6 @@ function TimeStats({ theme }) {
                             height={responsiveSizeHack}
                             options={{ ...options, ...containsGbp(stat.title) ? gbpTooltips : {} }} />
                         {stat.datasets.map(dataset =>
-
                             <ArrayRenderer
                                 key={dataset.label}
                                 rows={[stat.labels.reduce((acc, label, i) => ({ ...acc, [label]: dataset.data[i] }), {})]}
